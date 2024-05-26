a09a59662ab6d6c433312ce695be3c325f7f494d
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-05-26 13:23:08 +0300

    commited

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+  def __init__(self, name):
+    self.name = name
+
+  def match(self, word_list):
+    return ([w for w in word_list if self.is_anagram(w)])
+
+  def is_anagram(self, candidate):
+    candidate_lower = candidate.lower()
+    return candidate_lower != self.name and sorted(candidate_lower) == sorted(self.name)
+    
